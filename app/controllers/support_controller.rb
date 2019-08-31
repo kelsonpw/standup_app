804bc6f50c4336ffa53cd343340e3c9ecac9d5fc
Kelson Warner <kelson@null.net>
2019-08-31 12:28:36 -0700

    adding support

---
+++ app/controllers/support_controller.rb
@@ -0,0 +1,4 @@
+class SupportController < ApplicationController
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'support/contact'
   get "activity/mine"
   get "activity/feed"
 
