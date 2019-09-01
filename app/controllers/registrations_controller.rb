327c0396bb6d9ddc458e7e1fa7133b2fffd92a7d
Kelson Warner <kelson@null.net>
2019-09-01 10:36:02 -0700

    adding signup test

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_params
 
   protected
 
   def configure_permitted_params
-    devise_paramater_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
   def after_sign_up_path_for(resource)
